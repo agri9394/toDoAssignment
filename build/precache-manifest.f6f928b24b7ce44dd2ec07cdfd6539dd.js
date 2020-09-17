1846adaa62269e032f663339260469f941d06e41
Agrim Anand <agrim@grappus.com>
2020-09-17 22:25:26 +0530

    build Folder

---
+++ build/precache-manifest.f6f928b24b7ce44dd2ec07cdfd6539dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35451caa03bdd5925629f1da9424f91d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2147b504c5f3b49c46ce",
+    "url": "/static/css/main.3a300e77.chunk.css"
+  },
+  {
+    "revision": "9e3c635d2773c19edfba",
+    "url": "/static/js/2.487bf461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.487bf461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2147b504c5f3b49c46ce",
+    "url": "/static/js/main.ae20b970.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
